ff0c084eded8afaeb32c4798bcde03d8201f11e2
Karla <karla.nascimento@b2wdigital.com>
2020-02-26 17:35:40 -0300

    [RECURSIVIDADE]Corrige caso de recursão em somatorio

--- recursividade/Soma.c
+++ recursividade/Soma.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 
-int somatoria = 0;
-
-int main(void) {
-    int valores[5] = {2,4,6,7,8};
-
-    int tamanho = sizeof((valores)) / sizeof(int);
-    
-    for (int i = 0; i < tamanho; i++) {
-       somatoria =  somatorio(valores, i);
-    }
-
-    printf("%d\n", somatoria);
-    return 0;
-}
-
 int somatorio( int vetor[], int posicao) {
     if (posicao == 0) {
         return vetor[posicao];
@@ -22,3 +7,15 @@ int somatorio( int vetor[], int posicao) {
         return vetor[posicao] + somatorio(vetor, (posicao-1));
     }
 }
+
+int main(void) {
+    int valores[5] = {2,4,6,7,8};
+
+    int tamanho = sizeof((valores)) / sizeof(int);
+    
+    int somatoria =  somatorio(valores, tamanho);
+
+    printf("%d\n", somatoria);
+    return 0;
+}
+
