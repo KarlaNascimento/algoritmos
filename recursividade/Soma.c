e3402707bdc0ee5ec632a13787f44d5e18b42df5
Karla Nascimento <karla.nascimento01@outlook.com>
2020-02-26 00:36:50 -0300

    [RECURSIVIDADE]Exercicio de Soma

---
+++ recursividade/Soma.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int somatoria = 0;
+
+int main(void) {
+    int valores[5] = {2,4,6,7,8};
+
+    int tamanho = sizeof((valores)) / sizeof(int);
+    
+    for (int i = 0; i < tamanho; i++) {
+       somatoria =  somatorio(valores, i);
+    }
+
+    printf("%d\n", somatoria);
+    return 0;
+}
+
+int somatorio( int vetor[], int posicao) {
+    if (posicao == 0) {
+        return vetor[posicao];
+    } else {
+        return vetor[posicao] + somatudo(vetor, (posicao-1));
+    }
+}
